50a9128e990610ed0fc972b133a8129879e964c8
sivavanga <sn.vanga@gmail.com>
2026-08-10 22:17:55 +0530

    terraform-code

---
+++ aws-monitoring-platform/terraform/main.tf
@@ -0,0 +1,57 @@
+resource "aws_security_group" "monitoring" {
+  name        = "monitoring-server-sg"
+  description = "Security group for Prometheus and Grafana"
+
+  ingress {
+    description = "SSH"
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  ingress {
+    description = "Grafana"
+    protocol    = "tcp"
+    from_port   = 3000
+    to_port     = 3000
+    cidr_blocks = [var.allowed_grafana_cidr]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "monitoring-server-sg"
+  }
+}
+
+
+resource "aws_instance" "monitoring" {
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.monitoring.id
+  ]
+
+  user_data = file("${path.module}/user-data.sh")
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name        = "prometheus-grafana-server"
+    Environment = "dev"
+    Project     = "monitoring"
+  }
+}
---
+++ aws-monitoring-platform/terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.monitoring.id
+}
+
+output "public_ip" {
+  description = "EC2 public IP"
+  value       = aws_instance.monitoring.public_ip
+}
+
+output "grafana_url" {
+  description = "Grafana URL"
+  value       = "http://${aws_instance.monitoring.public_ip}:3000"
+}
+
+output "prometheus_internal_url" {
+  description = "Prometheus internal URL"
+  value       = "http://localhost:9090"
+}
